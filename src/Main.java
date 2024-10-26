9eb8442dcec877e540195a1f1dac2050ca591826
Gunayiskandarova <gunayiskandarova29@gmail.com>
2024-10-26 18:45:40 +0400

    EVEN-OLD

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,42 @@
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int number1 = 15;
+//        int number2 = 7;
+//
+//        int sum = number1 + number2;
+//        System.out.println (sum);
+//        int sub = number1-number2;
+//        System.out.println (sub);
+//        int multi = number1 * number2;
+//        System.out.println(multi);
+//        int per = number1 % number2;
+//        System.out.println(per);
+//        int div = number1/number2;
+//        System.out.println(div);
+//
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//public class NumberApp {
+//    public static void main(String[] args){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Number is:");
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = scanner.nextInt();
 
-        int number1 = 15;
-        int number2 = 7;
+        System.out.println(number % 2 == 0 ? "even" : "old");
 
-        int sum = number1 + number2;
-        System.out.println (sum);
-        int sub = number1-number2;
-        System.out.println (sub);
-        int multi = number1 * number2;
-        System.out.println(multi);
-        int per = number1 % number2;
-        System.out.println(per);
-        int div = number1/number2;
-        System.out.println(div);
-
+        scanner.close();
     }
 }
 
