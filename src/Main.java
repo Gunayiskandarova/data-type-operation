5c16855109e2a1e7ed1043d06a4ff11a735d2f55
Gunayiskandarova <gunayiskandarova29@gmail.com>
2024-10-26 18:09:40 +0400

    data-type-operation

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number1 = 15;
+        int number2 = 7;
+
+        int sum = number1 + number2;
+        System.out.println (sum);
+        int sub = number1-number2;
+        System.out.println (sub);
+        int multi = number1 * number2;
+        System.out.println(multi);
+        int per = number1 % number2;
+        System.out.println(per);
+        int div = number1/number2;
+        System.out.println(div);
+
+    }
+}
+
+
